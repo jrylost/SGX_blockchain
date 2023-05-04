581d0245247221db4a5871766b2b4597a6352ddb
jrylost <34121887+jrylost@users.noreply.github.com>
2023-05-04 18:49:51 +0800

    删除linux构建部分

---
+++ src/utils/attestation_deprocated.go
@@ -0,0 +1,34 @@
+package utils
+
+//
+//import (
+//	"crypto"
+//	"crypto/rand"
+//	"crypto/rsa"
+//	"crypto/x509"
+//	"crypto/x509/pkix"
+//	"fmt"
+//	"github.com/edgelesssys/ego/enclave"
+//	"math/big"
+//	"time"
+//)
+//
+//func CreateCertificate() ([]byte, crypto.PrivateKey) {
+//	template := &x509.Certificate{
+//		SerialNumber: &big.Int{},
+//		Subject:      pkix.Name{CommonName: "SGX_Blockchain"},
+//		NotAfter:     time.Now().AddDate(10, 0, 0),
+//		DNSNames:     []string{"localhost"},
+//	}
+//	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+//	cert, _ := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
+//	return cert, priv
+//}
+//
+//func GetRemoteReport(hash []byte) []byte {
+//	report, err := enclave.GetRemoteReport(hash[:])
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	return report
+//}
